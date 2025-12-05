ace183ce2c3ebf5e414117f0957a56285baa31cd
zbc <zbchun79@qq.com>
2025-12-05 17:05:02 +0800

    减少错误日志上报量,每个服务不超过1000行

--- cmd/all.go
+++ cmd/all.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	_ "costrict-keeper/cmd/client"
 	_ "costrict-keeper/cmd/component"
+	_ "costrict-keeper/cmd/misc"
 	_ "costrict-keeper/cmd/root"
 	_ "costrict-keeper/cmd/server"
 	_ "costrict-keeper/cmd/service"
